ee38ba2781c61be5a721aec3d883fdd67d8ee1f8
Jenn Ah <jennah@Jenns-Air.home>
2018-11-20 18:32:36 -1000

    migrated all 4 tables to db

--- server/db/migrations/20181120181138_create-priorities-table.js
+++ server/db/migrations/20181120181138_create-priorities-table.js
@@ -1,17 +1,15 @@
 
 exports.up = function(knex, Promise) {
-
-    return knex.schema.createTable('users', table => {
+    return knex.schema.createTable('priorities', table => {
         table.increments('id');
         table.unique('id');
-        table.string('first_name', 60).notNullable();
-        table.string('last_name', 60).notNullable();
-        table.string('email', 75).notNullable();
+        table.string('name', 60).notNullable();
+        table.integer('rank').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('users');
+    return knex.schema.dropTable('priorities');
 };
